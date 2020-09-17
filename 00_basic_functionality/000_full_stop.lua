7d28916d1f62921bdf37f011e2c0e737a1420455
Condor3397 <freedomcondor@126.com>
2020-09-17 16:20:24 +0200

    add basic functionality test codes and library test codes, report some issues in readme file

---
+++ 00_basic_functionality/000_full_stop.lua
@@ -0,0 +1,13 @@
+function init()
+end
+
+
+function step()
+   robot.differential_drive.set_target_velocity(0,0);
+end
+
+function reset()
+end
+
+function destroy()
+end
---
+++ 00_basic_functionality/001_lift_calibration.lua
@@ -0,0 +1,12 @@
+function init()
+   robot.lift_system.calibrate();
+end
+
+function step()
+end
+
+function reset()
+end
+
+function destroy()
+end
